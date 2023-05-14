930ba3d26395a270c458989df6b6186544c0b4a4
Alexandre-BARBIER <100125903+Alexandre-BARBIER@users.noreply.github.com>
2023-05-14 11:05:10 +0200

    Add files via upload

--- zsh_setup.sh
+++ zsh_setup.sh
@@ -1,22 +1,20 @@
 
-if [! -d "~/.config" ]; then
+if [ ! -d ~/.config ]; then
   mkdir ~/.config
 fi
 
-if [! -d "~/.config/zsh" ]; then
+if [ ! -d ~/.config/zsh ]; then
   mkdir ~/.config/zsh
 fi
 
 cd ~/.config/zsh
 
-git clone https://github.com/Alexandre-BARBIER/zsh-config.git
+git clone https://github.com/Alexandre-BARBIER/zsh-config.git ~/.config/zsh/zsh-config
 
-git clone https://github.com/zsh-users/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.config/zsh/zsh-autosuggestions
 
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.config/zsh/zsh-synthax-highlighting
 
-https://gitlab.viarezo.fr/ViaRezo/oh-my-viarezo.git
+git clone https://gitlab.viarezo.fr/ViaRezo/oh-my-viarezo.git ~/.config/zsh/zsh_theme/oh-my-viarezo
 
 ln -s ~/.config/zsh/zsh-config/.zshrc ~/.zshrc
-
-chsh -s $(which zsh)
